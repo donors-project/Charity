74cc7ec45eba5ae123188b4a040c116ecd9b3b95
07ASN <HASAN.OMAR.20002@GMAIL.COM>
2025-03-03 14:15:02 +0300

    Ramzi

--- frontend/src/components/LogIn/Login.jsx
+++ frontend/src/components/LogIn/Login.jsx
@@ -28,14 +28,13 @@ export default function Login() {
           "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
-         credentials: "include",
       });
 
       const data = await response.json();
 
       if (response.ok) {
         // Store token in HTTP-only cookie
-        // Cookies.set("token", data.token, { expires: 1 }); // Expires in 1 day
+        Cookies.set("token", data.token, { expires: 1 }); // Expires in 1 day
 
         setMessage("Login successful! Redirecting...");
         setTimeout(() => {
@@ -119,15 +118,7 @@ export default function Login() {
                 </label>
                 <div className="relative">
                   <span className="absolute inset-y-0 right-0 flex items-center pr-3 text-[#AAB99A]">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                    >
-                      <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                      <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-                    </svg>
+                    
                   </span>
                   <input
                     type="email"
